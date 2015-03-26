1c3fab2075c01e7a8b1b71a8506abd404933b0fd
kas-cor <kascorp@gmail.com>
2015-03-26 12:53:52 +0300

    Update rename-long-path, add path to arguments

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ My mini python utilities
 
 ## Rename-long-path
 Rename long paths for later delete
+```bash
+renlonpat.py path\to\folders1 [path\to\folders2] [path\to\folders3]
+```
--- Rename-long-path/renlonpat.py
+++ Rename-long-path/renlonpat.py
@@ -1,25 +1,36 @@
 import os
+import sys
 
 num=0
+folders=0
+files=0
 task=[]
 
-def show_all(sSrc):
+def show_all(sSrc,folders,files):
     for file in os.listdir(sSrc):
         file=os.path.join(sSrc,file)
         if os.path.isdir(file):
             task.append(file)
             print file
-            show_all(file)
+            folders+=1
+            folders,files=show_all(file,folders,files)
+        else:
+            files+=1
+    return folders,files
 
-show_all("E:\\Projects\\gulp-skeleton")
+for path in sys.argv[1:]:
+    folders,files=show_all(path,folders,files)
 
 print "=============="
+print "Total folders: %d" % folders
+print "Total files: %d" % files
+print
 
 task.reverse()
 for path in task:
     new=path.split("\\")
     old=new.pop()
-    if len(old)>3:
+    if not old.isdigit():
         new.append(str(num))
         num+=1
         to="\\".join(new)
